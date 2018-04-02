707f8e5f0ee6232d0aaa52d21d84508dcf9281ba
Cyril <consultant@seeren.fr>
2018-04-02 02:06:55 +0200

    Inject arguments to action method

--- src/HttpController.php
+++ src/HttpController.php
@@ -9,25 +9,26 @@
  *
  * @author (c) Cyril Ichti <consultant@seeren.fr>
  * @link https://github.com/seeren/controller
- * @version 1.2.3
+ * @version 2.0.1
  */
 
 namespace Seeren\Controller;
 
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
-use Seeren\Http\Request\ClientRequestInterface;
 use Seeren\Http\Request\ServerRequest;
 use Seeren\Http\Response\Response;
 use Seeren\Http\Response\ServerResponse;
 use Seeren\Model\Exception\ModelException;
 use Seeren\View\Exception\ViewException;
+use Seeren\Http\Stream\ServerResponseStream;
+use Seeren\View\ViewInterface;
+use Seeren\Container\Container;
+use ReflectionException;
+use ReflectionMethod;
 use BadMethodCallException;
 use RuntimeException;
 use Throwable;
-use Seeren\Http\Stream\ServerResponseStream;
-use Seeren\Model\ModelInterface;
-use Seeren\View\ViewInterface;
 
 /**
  * Class for represente http controller
@@ -53,14 +54,12 @@ class HttpController extends AbstractController implements HttpControllerInterfa
    /**
     * @param ServerRequestInterface $request http server request
     * @param ViewInterface $view view
-    * @param ModelInterface $model model
     */
    public function __construct(
        ServerRequest $request,
-       ViewInterface $view = null,
-       ModelInterface $model = null)
+       ViewInterface $view = null)
    {
-       parent::__construct($view, $model);
+       parent::__construct($view);
        $this->response = new ServerResponse(new ServerResponseStream);
        $this->request = $request;
    }
@@ -87,41 +86,35 @@ class HttpController extends AbstractController implements HttpControllerInterfa
     * {@inheritDoc}
     * @see \Seeren\Controller\HttpControllerInterface::execute()
     */
-   public function execute(): string
+   public function execute(Container $container = null): string
     {
        try {
            $this->response = $this->response->withHeader(
                Response::HEADER_CONTENT_TYPE,
-               $this->view->getContentType($this->request));
-           $this->__call($this->request->getAttribute("action", ""));
+               $this->view->getContentType($this->request)
+           );
+           $args = [];
+           $action = $this->request->getAttribute("action", "");
+           if ($container) {
+               foreach (
+                   (new ReflectionMethod($this, $action))->getParameters()
+                as $value) {
+                   $args[] = $container->get($value->getType()->__toString());
+               }
+           }
+           $this->__call($action, $args);
            return $this->view->render();
        } catch (Throwable $e) {
            $this->response = $e instanceof ViewException
                            ? $this->response->withStatus(406)
                            : ($e instanceof BadMethodCallException
+                           || $e instanceof ReflectionException
                            ? $this->response->withStatus(405)
                            : ($e instanceof ModelException
                            ? $this->response->withStatus(404)
                            : $this->response->withStatus(500)));
            throw new RuntimeException(
                "Can't execute " . static::class . ": " .$e->getMessage());
-       }
-    }
-
-   /**
-    * {@inheritDoc}
-    * @see \Seeren\Controller\HttpControllerInterface::consume()
-    */
-   public function consume(
-       ClientRequestInterface $client,
-       string $requestTarget): ResponseInterface
-    {
-       try {
-           return $client->withRequestTarget($requestTarget)->getResponse();
-       } catch (Throwable $e) {
-           throw new RuntimeException(
-               "Can't " . static::class . ":consume "
-             . $requestTarget . ": " . $e->getMessage());
        }
     }
 
