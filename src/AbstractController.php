dfe2614aceb17bd1c29e87f5f65d2ede232344ef
Cyril <consultant@seeren.fr>
2018-04-02 02:04:47 +0200

    Remove model

--- src/AbstractController.php
+++ src/AbstractController.php
@@ -1,22 +1,19 @@
 <?php
 
 /**
- * This file contain Seeren\Controller\Controller class
  *     __
  *    / /__ __ __ __ __ __
  *   / // // // // // // /
  *  /_// // // // // // /
  *    /_//_//_//_//_//_/
  *
- * @copyright (c) Cyril Ichti <consultant@seeren.fr>
+ * @author (c) Cyril Ichti <consultant@seeren.fr>
  * @link https://github.com/seeren/controller
- * @version 1.1.1
+ * @version 2.0.1
  */
 
 namespace Seeren\Controller;
 
-use Seeren\Model\Model;
-use Seeren\Model\ModelInterface;
 use Seeren\View\ViewInterface;
 use Seeren\View\Observer\AbstractSubject;
 use Seeren\View\View;
@@ -33,45 +30,17 @@ class AbstractController extends AbstractSubject
    protected
 
        /**
-        * @var ModelInterface
-        */
-       $model,
-       /**
         * @var ViewInterface
         */
        $view;
 
    /**
     * @param ViewInterface $view view
-    * @param ModelInterface $model model
     */
    protected function __construct(
-       ViewInterface $view = null,
-       ModelInterface $model = null)
+       ViewInterface $view = null)
    {
        $this->view = $view ? $view : new View;
-       if ($model) {
-           $this->setModel($model);
-       }
-   }
-
-   /**
-    * @return ModelInterface
-    */
-   private final function setModel(ModelInterface $model): ModelInterface
-   {
-       $this->model = $model;
-       $this->model->attach($this->view);
-       return $model;
-   }
-
-   /**
-    * {@inheritDoc}
-    * @see \Seeren\Controller\ControllerInterface::getModel()
-    */
-   public final function getModel(): ModelInterface
-   {
-       return $this->model ? $this->model : $this->setModel(new Model);
    }
 
    /**
